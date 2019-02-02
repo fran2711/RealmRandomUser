3ca3f787c78e742f1f614caa49a2fbf0a31a5dcf
Fran Lucena <fjlucena@minsait.com>
2019-02-02 09:19:37 +0100

    Initial Commit

---
+++ RandomCoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RandomCoApp
+//
+//  Created by Francisco Lucena de Juan on 02/02/2019.
+//  Copyright © 2019 Francisco Lucena de Juan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
